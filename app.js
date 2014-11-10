c8ad80a1be8951bb3aa783617783907532ee3a6d
Gilberto Avalos <gilberto@ooyala.com>
2014-11-10 15:28:37 -0600

    Add vr-controller provider

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 'use strict';
 
-var express = require('express');
-var http = require('http');
-var app = express();
-var io = require('socket.io')({
-  'transports': ['xhr-polling'],
-  'polling duration': 10
-});
+var express = require('express'),
+  http = require('http'),
+  app = express(),
+  fs = require('fs'),
+  io = require('socket.io')({
+    'transports': ['xhr-polling'],
+    'polling duration': 10
+  });
 
+// Static content
 app.use(express.static(__dirname + '/public'));
+
+// Library provider
+app.get('/library', function(req, res) {
+  res.setHeader('Content-Type', 'application/javascript');
+
+  var script = __dirname + '/client/vr-controller.js';
+  var fileStream = fs.createReadStream(script);
+  fileStream.pipe(res);
+});
 
 var server = http.createServer(app).listen(8080);
 var sockets = io.listen(server);
@@ -29,6 +40,7 @@ sockets.on('connection', function(socket) {
       ping();
     }, 1000);
   };
+
   ping();
 
 });
